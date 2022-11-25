64106cfd847395853744190354a964a1f7e71ef8
Kyrexa <108000953+Kyrexa@users.noreply.github.com>
2022-11-25 16:42:15 +0300

    Update Program.cs

--- FinalWork_1/Program.cs
+++ FinalWork_1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Итоговая проверочная работа 1 блок
+//Например:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+Console.Clear();
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+string[] FindLessThan(string[] input, int c) {
+    string[] output = new string[CountLessThan(input, c)];
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= c) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+int CountLessThan(string[] input, int c) {
+    int count = 0;
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= c) {
+            count++;
+        }
+    }
+    return count;
+}
+string[] AskArray() {
+    Console.Write("Вводите через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
